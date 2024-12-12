dbb455268985845fc19bedadc796990065e40ac0
sergiooo74 <sergey.rybakov@mail.ru>
2024-12-12 21:20:49 +0300

    from12/12/24

--- users/forms.py
+++ users/forms.py
@@ -5,7 +5,7 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class UserRegistrationForm(forms.ModelForm):
-    password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Повторите пароль', widget=forms.PasswordInput)
 
     def clean_password2(self):
         cleaned_data = self.cleaned_data
